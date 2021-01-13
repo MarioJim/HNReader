489a69e22c5637596670185a4efc89704f5f701f
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-01-12 22:51:30 -0600

    Add Comments Activity and Comment Fragment

---
+++ app/src/main/java/org/team4/hnreader/CommentsActivity.kt
@@ -0,0 +1,11 @@
+package org.team4.hnreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommentsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comments)
+    }
+}
--- app/src/main/java/org/team4/hnreader/PostFragment.kt
+++ app/src/main/java/org/team4/hnreader/PostFragment.kt
@@ -1,15 +1,20 @@
 package org.team4.hnreader
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_post.*
 
-import kotlinx.android.synthetic.main.fragment_post.*;
 
 private const val POST_TITLE = "Título de post"
-private const val POST_URL = "news.ycombinator.com"
+private const val POST_URL = "https://news.ycombinator.com"
 private const val POST_USER = "Usuario"
 
 class PostFragment : Fragment() {
@@ -34,7 +39,38 @@ class PostFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        val view = inflater.inflate(R.layout.fragment_post, container, false)
+        val urlBtn = view.findViewById<ConstraintLayout>(R.id.urlBtn)
+        val commentsBtn = view.findViewById<ConstraintLayout>(R.id.commentsBtn)
+        val shareButton = view.findViewById<Button>(R.id.shareBtn)
+        val saveButton = view.findViewById<Button>(R.id.saveBtn)
+
+        urlBtn.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://news.ycombinator.com"))
+            startActivity(browserIntent)
+        }
+
+        commentsBtn.setOnClickListener {
+            val intentToComments = Intent(this.activity, CommentsActivity::class.java)
+            startActivity(intentToComments)
+        }
+
+        shareButton.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, postURL)
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        saveButton.setOnClickListener {
+            Toast.makeText(this.activity, "Saved item", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 
     companion object {
